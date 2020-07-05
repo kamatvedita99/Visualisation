decf329c5f437fc4092708a98be2df10d6e6007d
sid <15kamat1999@gmail.com>
2020-07-05 22:13:06 +0530

    Added code

---
+++ Visualisation/tabs/sidepanel.py
@@ -0,0 +1,22 @@
+import dash
+import plotly
+import dash_core_components as dcc
+import dash_html_components as html 
+import dash_bootstrap_components as dbc 
+import dash_table
+import pandas
+from dash.dependencies import Input, Output
+
+from app import app
+
+from tabs import tab1, tab2
+from database import transforms
+
+df = transforms.df
+layout = html.Div([
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
+])
